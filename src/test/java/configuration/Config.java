f890ae76dfe5d618a2f43afa6ab3015df3d94540
Matheus Bandeira de Oliveira <bandeiramatheus@hotmail.com>
2020-08-17 20:14:55 -0300

    Primeiro commit

---
+++ src/test/java/configuration/Config.java
@@ -0,0 +1,37 @@
+package configuration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Config {
+
+    private WebDriver driver;
+
+    public WebDriver iniciaDriver(){
+        String soNome = System.getProperty("os.name");
+        if(soNome.startsWith("W")){
+            System.setProperty("webdriver.chrome.driver","drivers/windows.exe");
+            driver = new ChromeDriver();
+        }else if(soNome.startsWith("M")){
+            System.setProperty("webdriver.chrome.driver","drivers/mac");
+            driver = new ChromeDriver();
+        } else if(soNome.startsWith("U") || soNome.startsWith("L")) {
+            System.setProperty("webdriver.chrome.driver", "drivers/linux");
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public WebDriver acessaUrl(String nomeSite){
+        WebDriver driver = iniciaDriver();
+        driver.get("https://"+nomeSite+".com.br");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        return driver;
+    }
+
+
+
+}
